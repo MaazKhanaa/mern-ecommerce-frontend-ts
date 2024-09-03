cd28cd04e7a1439b2faa8de1a019ac31639a7d97
Maaz Khan <maxiikhan04@gmail.com>
2024-09-03 15:54:29 +0500

    added react memo to the generic components

--- src/common/button.tsx
+++ src/common/button.tsx
@@ -1,8 +1,11 @@
-import { ButtonProps } from 'src/interfaces'
-import '../styles/common.css'
+import React from 'react';
+import { ButtonProps } from 'src/interfaces';
+import '../styles/common.css';
 
-export const Button: React.FC<ButtonProps> = ({text, onClick, type = 'button', disabled = false, className = ''}) => {
+export const Button: React.FC<ButtonProps> = React.memo(({ text, onClick, type = 'button', disabled = false, className = '' }) => {
     return (
-        <button className={`btn primary-btn ${className}`} type={type} onClick={onClick} disabled={disabled}>{text}</button>
-    )
-}
+        <button className={`btn primary-btn ${className}`} type={type} onClick={onClick} disabled={disabled}>
+            {text}
+        </button>
+    );
+});
