7d73319d5849844d9d18a05692979da1b55613b5
Maaz Khan <maxiikhan04@gmail.com>
2024-09-03 16:02:31 +0500

    footer updated

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
     return (
-        <div className="main-footer">
-            <h3 className="text-center">Footer</h3>
+        <div className="p-3 border border-top">
+            <p className="mb-0 text-center text-secondary">&copy; Copywrite E-Commerce 2024</p>
         </div>
     );
 }
